24410c52f7814eed8fb3c1d94c99e72b453045c5
unknown <waibaniraj208@gmail.com>
2023-11-13 21:48:38 +0545

    Playing with home page and pagination

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -10,6 +10,18 @@ function HomePage() {
   const [categories, setCategories] = useState([]);
   const [checked, setChecked] = useState([]);
   const [radio, setRadio] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const getTotal = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:8080/api/v1/product/product-count"
+      );
+      setTotal(data?.total);
+    } catch (error) {}
+  };
 
   const getAllCategory = async () => {
     try {
@@ -26,22 +38,49 @@ function HomePage() {
   };
   useEffect(() => {
     getAllCategory();
+    getTotal();
   }, []);
   // get products
   const getAllProducts = async () => {
     try {
+      setLoading(true);
       const { data } = await axios.get(
-        "http://localhost:8080/api/v1/product/get-product"
+        `http://localhost:8080/api/v1/product/product-list/${page}`
       );
+      setLoading(false);
       setProducts(data.products);
     } catch (error) {
+      setLoading(false);
       console.log(error);
       toast.error("Some went wrong to get all the products");
     }
   };
+
   useEffect(() => {
-    getAllProducts();
-  }, []);
+    if (page == 1) return;
+    loadMore();
+  }, [page]);
+  // load more
+  const loadMore = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(
+        `http://localhost:8080/api/v1/product/product-list/${page}`
+      );
+      setLoading(false);
+      setProducts([...products, ...data?.products]);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!checked.length || !radio.length) getAllProducts();
+  }, [checked.length, radio.length]);
+  useEffect(() => {
+    if (checked.length || radio.length) filterProduct();
+  }, [checked, radio]);
 
   // filter by category
   const handleFilter = (value, id) => {
@@ -52,6 +91,17 @@ function HomePage() {
       all = all.filter((c) => c !== id);
     }
     setChecked(all);
+  };
+
+  //get Filter product
+  const filterProduct = async () => {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:8080/api/v1/product/product-filters",
+        { checked, radio }
+      );
+      setProducts(data?.products);
+    } catch (error) {}
   };
   return (
     <Layout title={"All Products - Best offers"}>
@@ -79,11 +129,14 @@ function HomePage() {
               ))}
             </Radio.Group>
           </div>
+          <button
+            className="btn btn-danger"
+            onClick={() => window.location.reload()}
+          >
+            Reset Filters
+          </button>
         </div>
         <div className="col-md-9">
-          {JSON.stringify(checked, null, 4)}
-          {JSON.stringify(radio, null, 4)}
-
           <h1 className="text-center">All Products</h1>
           <div className="d-flex flex-wrap">
             {products?.map((p) => (
@@ -95,7 +148,8 @@ function HomePage() {
                 />
                 <div className="card-body">
                   <h5 className="card-title">{p.name}</h5>
-                  <p className="card-text">{p.description}</p>
+                  <p className="card-text">{p.description.substring(0, 20)}</p>
+                  <p className="card-text">Rs. {p.price}</p>
                   <button className="btn btn-primary ms-1">More Details</button>
                   <button className="btn btn-secondary ms-1">
                     Add to Cart
@@ -103,6 +157,19 @@ function HomePage() {
                 </div>
               </div>
             ))}
+          </div>
+          <div className="m-2 p-3">
+            {products && products.length < total && (
+              <button
+                className="btn btn-warning"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setPage(page + 1);
+                }}
+              >
+                {loading ? "Loading.. " : "Loadmore"}
+              </button>
+            )}
           </div>
         </div>
       </div>
