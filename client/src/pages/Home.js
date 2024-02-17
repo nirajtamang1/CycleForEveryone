7f9da82f706dbdb48dbe08f38e108dc8ee1147e3
unknown <waibaniraj208@gmail.com>
2024-02-17 23:23:13 +0545

    Remove node

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -22,30 +22,6 @@ function Home() {
       console.log(error);
     }
   };
-  for (let i = 0; i < products.length; i++) {
-    const p = products[i];
-    productCards.push(
-      <div className="card m-3 shadow" style={{ width: "18rem" }} key={p._id}>
-        <img
-          className="card-img-top"
-          src={`/api/v1/product/product-photo/${p._id}`}
-          alt={p.name}
-          style={{ height: "150px" }}
-        />
-        <div className="card-body">
-          <h5 className="card-title">{p.name}</h5>
-          <p className="card-text">{p.description.substring(0, 20)}</p>
-          <p className="card-text">Rs. {p.price}</p>
-          <button
-            className="cartbtn ms-1 w-100"
-            onClick={() => navigate(`/product/${p.slug}`)}
-          >
-            View More
-          </button>
-        </div>
-      </div>
-    );
-  }
 
   return (
     <Layout title={"Home Page-Cycle Booking System"}>
@@ -61,7 +37,33 @@ function Home() {
               making it a Class 3 e-bike.
             </p>
           </div>
-          <div className="d-flex flex-wrap">{productCards}</div>
+          <div className="d-flex flex-wrap">
+            {products.map((p) => (
+              <div
+                className="card m-3 shadow"
+                style={{ width: "18rem" }}
+                key={p._id}
+              >
+                <img
+                  className="card-img-top"
+                  src={`/api/v1/product/product-photo/${p._id}`}
+                  alt={p.name}
+                  style={{ height: "150px" }}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.description.substring(0, 20)}</p>
+                  <p className="card-text">Rs. {p.price}</p>
+                  <button
+                    className="cartbtn ms-1 w-100"
+                    onClick={() => navigate(`/product/${p.slug}`)}
+                  >
+                    View More
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
         </section>
         <Aboutus />
         <Contactus />
